6ef8624bb1f912ac04da27cac2dc82682a7ed9c5
Olaniyan-tech <alliolaniyan1@gmail.com>
2026-04-03 01:02:38 +0100

    Create permissions for the vendors

--- food/permissions.py
+++ food/permissions.py
@@ -14,3 +14,32 @@ class IsStaff(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_staff
     
+class IsVendor(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated and
+            hasattr(request.user, "vendor")
+        )
+
+class IsApprovedVendor(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated and
+            hasattr(request.user, "vendor") and
+            request.user.vendor.is_approved and
+            request.user.vendor.is_active
+        )
+
+class IsVendorOrderOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if not hasattr(request.user, "vendor"):
+            return False
+        return obj.vendor == request.user.vendor
+
+class IsStaffOrVendorOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        if not hasattr(request.user, "vendor"):
+            return False
+        return obj.vendor == request.user.vendor
