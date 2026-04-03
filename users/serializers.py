ac62951ebfd864b719cc339196735087232c9a92
Olaniyan-tech <alliolaniyan1@gmail.com>
2026-04-03 01:07:18 +0100

    Validate users profile

--- users/serializers.py
+++ users/serializers.py
@@ -53,3 +53,39 @@ class RegisterSerializer(serializers.ModelSerializer):
         Profile.objects.create(user=user, phone=phone)
 
         return user
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user.username", required=True)
+    email = serializers.EmailField(source="user.email", required=True)
+    phone = serializers.CharField(required=False)
+
+    class Meta:
+        model = Profile
+        fields = ("id", "username", "email", "phone")
+        read_only_fields = ("id",)
+    
+    def validate_email(self, value):
+        if User.objects.filter(email__iexact=value).exclude(id=self.instance.user.id).exists():
+            raise ValidationError("Email already exists")
+        return value
+    
+    def validate_phone(self, value):
+        validate_phone_format(value)
+        if Profile.objects.filter(phone=value).exclude(id=self.instance.user.id).exists():
+            raise serializers.ValidationError("Phone number already exists")
+        return value
+
+    def update(self, instance, validated_data):
+        # Update User fields
+        user_data = validated_data.pop("user", {})
+        user = instance.user
+        for attr, value in user_data.items():
+            setattr(user, attr, value)
+        user.save()
+
+        # Update Profile fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
