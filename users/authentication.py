e5a3ad3e7c705f64894f5deeca6af3d115262831
Olaniyan-tech <alliolaniyan1@gmail.com>
2026-04-03 01:11:15 +0100

    Handle users authentication

--- users/authentication.py
+++ users/authentication.py
@@ -1,4 +1,5 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from drf_spectacular.extensions import OpenApiAuthenticationExtension
 
 class CookieJWTAuthentication(JWTAuthentication):
@@ -6,8 +7,13 @@ class CookieJWTAuthentication(JWTAuthentication):
           raw_token = request.COOKIES.get("access_token")
           if raw_token is None:
                return None
-          validated_token = self.get_validated_token(raw_token)
-          return self.get_user(validated_token), validated_token
+          
+          try:
+            validated_token = self.get_validated_token(raw_token)
+            return self.get_user(validated_token), validated_token
+          
+          except (TokenError, InvalidToken):
+              return None
 
 class CookieJWTAuthenticationExtension(OpenApiAuthenticationExtension):
     target_class = "users.authentication.CookieJWTAuthentication"
